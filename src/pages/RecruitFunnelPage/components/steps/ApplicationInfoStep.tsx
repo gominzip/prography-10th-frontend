8095e7fe62ace788132cdeb27a0a73236ed4311b
gominzip <ahaminji@naver.com>
2025-02-19 02:41:38 +0900

    feat: 지원 정보 스텝 구현

---
+++ src/pages/RecruitFunnelPage/components/steps/ApplicationInfoStep.tsx
@@ -0,0 +1,23 @@
+import type { StepProps } from '@/types/form';
+import { FieldSet, RadioField } from '../fields';
+import Section from '../fields/Section';
+import { partFieldMapping } from '@/constants/form';
+
+function ApplicationInfoStep({ register, errors, showError }: StepProps) {
+  const parts = Object.keys(partFieldMapping) as Array<keyof typeof partFieldMapping>;
+
+  return (
+    <Section title="지원 정보" description="지원하고자 하는 분야를 선택해주세요">
+      <FieldSet
+        legend="지원 분야를 선택해주세요"
+        warningText={showError && errors.selectedOption ? errors.selectedOption.message : undefined}
+      >
+        {parts.map((label) => (
+          <RadioField key={label} label={label} value={partFieldMapping[label]} {...register('selectedOption')} />
+        ))}
+      </FieldSet>
+    </Section>
+  );
+}
+
+export default ApplicationInfoStep;
