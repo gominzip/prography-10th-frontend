5d500335b5766b720a81daa1e1c771f0af19b483
gominzip <ahaminji@naver.com>
2025-02-20 02:38:29 +0900

    feat: 자식노드에 포커스 이벤트 발생 시 FieldSet border 색상 변경

--- src/pages/RecruitFunnelPage/components/fields/FieldSet.tsx
+++ src/pages/RecruitFunnelPage/components/fields/FieldSet.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from 'react';
+
 interface FieldSetProps {
   legend: string;
   description?: string;
@@ -6,8 +8,55 @@ interface FieldSetProps {
 }
 
 function FieldSet({ legend, description, warningText, children }: FieldSetProps) {
+  const [isFocused, setIsFocused] = useState(false);
+  const fieldsetRef = useRef<HTMLFieldSetElement>(null);
+
+  // 현재 유저가 진행 중인 필드 식별을 위한 이벤트 추가
+  useEffect(() => {
+    const fieldset = fieldsetRef.current;
+    if (!fieldset) return;
+
+    const handleFocus = (event: FocusEvent) => {
+      if ((event.target as HTMLElement).classList.contains('field')) {
+        setIsFocused(true);
+      }
+    };
+
+    const handleBlur = (event: FocusEvent) => {
+      if ((event.target as HTMLElement).classList.contains('field')) {
+        setIsFocused(false);
+      }
+    };
+
+    const handleClick = (event: MouseEvent) => {
+      if ((event.target as HTMLElement).classList.contains('field')) {
+        setIsFocused(true);
+      } else {
+        setIsFocused(false);
+      }
+    };
+
+    const fields = fieldset.querySelectorAll('.field');
+    fields.forEach((field) => {
+      field.addEventListener('focus', handleFocus as EventListener);
+      field.addEventListener('blur', handleBlur as EventListener);
+      field.addEventListener('click', handleClick as EventListener);
+    });
+
+    return () => {
+      fields.forEach((field) => {
+        field.removeEventListener('focus', handleFocus as EventListener);
+        field.removeEventListener('blur', handleBlur as EventListener);
+        field.removeEventListener('click', handleClick as EventListener);
+      });
+    };
+  }, []);
+
   return (
-    <fieldset className="p-6 rounded-xl border border-gray-300 bg-white">
+    <fieldset
+      ref={fieldsetRef}
+      className={`p-6 rounded-xl border ${isFocused ? 'border-blue-500' : 'border-gray-300'} bg-white`}
+    >
       {description && <p className="mb-3 text-sm font-semibold text-gray-700 whitespace-pre-line">{description}</p>}
 
       <legend className="px-2 mb-2 text-sm font-semibold text-gray-700 required">{legend}</legend>
